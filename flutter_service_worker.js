14f5b0a281d2a555adbf8f2bb4079c4ce4689b55
Axelkarnebro <axekrn-7@student.ltu.se>
2022-12-08 13:32:10 +0100

    what

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,8 +53,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a91d8e847213f28fc1c336eb3de3720f",
-"/": "a91d8e847213f28fc1c336eb3de3720f",
+"index.html": "884d959d5540e8025679f31c5a67a048",
+"/": "884d959d5540e8025679f31c5a67a048",
 "main.dart.js": "3c0bafd187213f357fbe713babcaaaf4",
 "manifest.json": "0d03a559b264393f0fcbe319a09d124e",
 "version.json": "5e8088e9f864a3bb92ffda25372adca7"
